de09c1021562caee5f948296bd3cbc3e19752bbd
--local <--local>
2016-08-25 11:49:38 +0300

    Small correction on first exercise

--- 1_say_hello.js
+++ 1_say_hello.js
@@ -5,7 +5,7 @@ stdin.resume();
 stdin.setEncoding('utf8');
 
 stdout.write('What is your name? ');
-process.stdin.on('data', function (text) {
+stdin.on('data', function (text) {
 	console.log('Hello, ' + text.toString().trim() + ', nice to meet you!');
 	process.exit();
 });
